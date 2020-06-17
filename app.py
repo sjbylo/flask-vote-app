6cad14ac2d3df2d7bde8c4eb4d218855c2c8eaa4
Logan Ballard <loganballard@gmail.com>
2020-06-17 12:44:26 -0700

    adding mysql example

--- app.py
+++ app.py
@@ -20,16 +20,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #dbpass  = os.environ.get('DB_PASS', '')
 #dbtype  = os.environ.get('DB_TYPE', '')
 
-# These are the var names created via the RDS binding using the AWS Broker 
-# DB_NAME: 
-# ENDPOINT_ADDRESS: 
-# MASTER_PASSWORD: 
-# MASTER_USERNAME: 
-# PORT:
-
-# The default values ('db', 'vote' etc) are for making it easier to create this app in the OpenShift console after 
-# setting up a DB call 'db'...
-
 dbhost  = os.environ.get('ENDPOINT_ADDRESS', 'db')
 dbport  = os.environ.get('PORT', '3306')
 dbname  = os.environ.get('DB_NAME', 'vote')
